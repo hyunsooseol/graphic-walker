207d2c02b9c1955715597ec46638d42afc764dda
hyunsooseol <snow@cau.ac.kr>
2025-07-06 15:31:25 +0900

    Updates

---
+++ fields/filterField/filterPill.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="@/global" />
+import React from 'react';
+import { DraggableProvided } from '@kanaries/react-beautiful-dnd';
+interface FilterPillProps {
+    provided: DraggableProvided;
+    fIndex: number;
+}
+declare const FilterPill: React.FC<FilterPillProps>;
+export default FilterPill;
---
+++ fields/obComponents/obFContainer.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="@/global" />
+import React from 'react';
+import { DroppableProvided } from '@kanaries/react-beautiful-dnd';
+import { IDraggableViewStateKey } from '../../interfaces';
+interface FieldContainerProps {
+    provided: DroppableProvided;
+    /**
+     * draggable Field Id
+     */
+    dkey: IDraggableViewStateKey;
+}
+declare const _default: React.FunctionComponent<FieldContainerProps>;
+export default _default;
